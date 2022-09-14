953b3a3b3116acf3ff2edaa547cbc047243a7024
Gintaras Leon <leogintaz@gmail.com>
2022-09-14 21:47:50 +0300

    comit

--- Atsiskaitymai/Tower of Hanoi/Game.cs
+++ Atsiskaitymai/Tower of Hanoi/Game.cs
@@ -8,45 +8,112 @@ namespace Tower_of_Hanoi
 {
     internal class Game
     {
-        private string handReset = "";
+        private string hand;
+        private string handReset = "         ";
         
         public void Start(List<Tower> towers, List<string> disks)
         {
             Input input = new Input();
             towers[0].lines = disks;
-          
+
 
             while (true)
             {
                 towers[0].Draw();
                 towers[1].Draw();
                 towers[2].Draw();
+                
+                //---------Take-------------//
                 switch (input.Read())
                 {
                     case 1:
                         for (int i = 0; i < disks.Count; i++)
                         {
-                            if (disks[i].Length < disks[i+1].Length)
+                            if (i == (disks.Count - 1) || disks[i].Length < disks[i+1].Length)
                             {
                                 Hand(disks[i]);
                                 towers[0].lines[i] = towers[0].lineReset;
-                                
+
                                 break;
                             }
                         }
                         break;
                     case 2:
+                        for (int i = 0; i < disks.Count; i++)
+                        {
+                            if (i == (disks.Count - 1) || disks[i].Length < disks[i + 1].Length)
+                            {
+                                Hand(disks[i]);
+                                towers[1].lines[i] = towers[1].lineReset;
+
+                                break;
+                            }
+                        }
                         break;
                     case 3:
+                        for (int i = 0; i < disks.Count; i++)
+                        {
+                            if (i == (disks.Count - 1) || disks[i].Length < disks[i + 1].Length)
+                            {
+                                Hand(disks[i]);
+                                towers[2].lines[i] = towers[2].lineReset;
+
+                                break;
+                            }
+                        }
                         break;
                     case -1:
                         break;
                 }
+
+                //---------Place-------------//
+                towers[0].Draw();
+                towers[1].Draw();
+                towers[2].Draw();
+
+                switch (input.Read())
+                    {
+                        case 1:
+                            for (int i = 0; i < disks.Count; i++)
+                            {
+                                
+                                //if ()
+                                    Hand(disks[i]);
+                                    //towers[1].lines[i] = towers[0].lineReset;
+
+                                    break;
+                                
+                            }
+                            break;
+                        case 2:
+                            for (int i = 3; i < disks.Count; i--)
+                            {
+                            if (!towers[1].lines[i].Contains("#"))
+                            {
+                                towers[1].lines[i] = hand;
+                                Hand(handReset);
+                                
+                                break;
+                            }
+
+
+                                
+
+                            }
+                            break;
+                        case 3:
+                            break;
+                        case -1:
+                            break;
+                    }
+
+                
             }
 
         }
         private void Hand(string disk) 
-        { 
+        {
+            hand = disk;
             Console.SetCursorPosition(30, 7);
             Console.WriteLine(disk);
         }
